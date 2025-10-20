61d70c8c243cf6e665827c951acfdd63a78a4d64
Bianca Mateiu <mateiu.bianca@gmail.com>
2025-10-20 18:01:25 +0300

    chore: Setup Tailwind

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,5 +1,5 @@
 import { getClosestCoffeeShopsWithDistance } from '@app/services/coffee-shops.service'
 
 export default async function Page() {
-  return <h1>Hello, Next.js!</h1>
+  return <h1 className="font-bold text-red-500">Hello, Next.js!</h1>
 }
